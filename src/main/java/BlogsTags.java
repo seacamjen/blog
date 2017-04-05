87c65f12d372fdcd193bf5bdfa079b6f36ab34b5
Cameron Jensen and Chance Magno <chance@>
2017-04-05 11:15:28 -0700

    add methods to BlogsTags class

--- src/main/java/BlogsTags.java
+++ src/main/java/BlogsTags.java
@@ -8,9 +8,64 @@ import java.util.TimerTask;
 import java.sql.Timestamp;
 
 public class BlogsTags {
+  private int tag_id;
+  private int blog_id;
+  private int id;
 
   public BlogsTags(int tag_id, int blog_id) {
+    this.tag_id = tag_id;
+    this.blog_id = blog_id;
+  }
 
+  public int getTagId() {
+    return tag_id;
+  }
+
+  public int getBlogId() {
+    return blog_id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO blogs_tags (tag_id, blog_id) VALUES (:tag_id, :blog_id);";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("tag_id", tag_id)
+      .addParameter("blog_id", blog_id)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static List<BlogsTags> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM blogs_tags;";
+      return con.createQuery(sql)
+      .executeAndFetch(BlogsTags.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherBlogsTags) {
+    if (!(otherBlogsTags instanceof BlogsTags)) {
+      return false;
+    } else {
+      BlogsTags newBlogsTags = (BlogsTags) otherBlogsTags;
+      return this.getTagId() == newBlogsTags.getTagId() &&
+             this.getBlogId() == newBlogsTags.getBlogId();
+    }
+  }
+
+  public static BlogsTags find(int id) {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM blogs_tags WHERE id = :id;";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(BlogsTags.class);
+    }
   }
 
 }
--- src/test/java/BlogsTagsTest.java
+++ src/test/java/BlogsTagsTest.java
@@ -16,19 +16,60 @@ public class BlogsTagsTest {
 
   @Test
   public void blog_instantiatesCorrectly_true() {
-    Blog newBlog = new Blog("Sally", "Great Blog", "blah blah blah");
-    assertTrue(newBlog instanceof Blog);
+    BlogsTags newBlogsTags = new BlogsTags(1, 2);
+    assertTrue(newBlogsTags instanceof BlogsTags);
   }
 
-  // @Test
-  // public void getAuthor_instantiatesWithAuthor_true() {
-  //   Blog newBlog = new Blog("Sally", "Great Blog", "blah blah blah");
-  //   assertEquals("Sally", newBlog.getAuthor());
-  // }
-  //
-  // @Test
-  // public void getAuthor_instantiatesWithAuthor_true() {
-  //   Blog newBlog = new Blog("Sally", "Great Blog", "blah blah blah");
-  //   assertEquals("Sally", newBlog.getAuthor());
-  // }
+  @Test
+  public void getTagId_instantiatesWithTagId_true() {
+    BlogsTags newBlogsTags = new BlogsTags(1, 2);
+    assertEquals(1, newBlogsTags.getTagId());
+  }
+
+  @Test
+  public void getBlogId_instantiatesWithBlogId_true() {
+    BlogsTags newBlogsTags = new BlogsTags(1, 2);
+    assertEquals(2, newBlogsTags.getBlogId());
+  }
+
+  @Test
+  public void save_savestag_true() {
+    Tag newTag = new Tag("Arnold");
+    newTag.save();
+    assertTrue(Tag.all().get(0).equals(newTag));
+  }
+
+  @Test
+  public void all_returnsAllTag_true() {
+    Tag newTag1 = new Tag("Arnold");
+    newTag1.save();
+    Tag newTag2 = new Tag("BArnold");
+    newTag2.save();
+    assertEquals(Tag.all().get(0), newTag1);
+    assertEquals(Tag.all().get(1), newTag2);
+  }
+
+  @Test
+  public void equals_comparesTagBasedOnName_true() {
+    Tag newtag = new Tag("Arnold");
+    Tag newtag1 = new Tag("Arnold");
+    assertTrue(newtag.equals(newtag1));
+  }
+
+  @Test
+  public void getId_returnsIdForTag_true() {
+    Tag newtag = new Tag("Arnold");
+    newtag.save();
+    assertTrue(newtag.getId() > 0);
+  }
+
+  @Test
+  public void find_returnsTagWithGivenId_true() {
+    Tag newTag = new Tag("arnold");
+    newTag.save();
+    Tag otherTag = new Tag("arnold");
+    otherTag.save();
+    assertEquals(otherTag, Tag.find(otherTag.getId()));
+    assertEquals(newTag, Tag.find(newTag.getId()));
+  }
 }
